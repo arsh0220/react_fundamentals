663865a3ef3df5dbce076408607704395eec6fe3
Arshdeep Kaur <kaurarshdeep1100@gmail.com>
2025-01-23 15:06:14 +0530

    first commit

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const About = () => {
+    return(
+    <div>
+        <h1>About Us</h1>
+        <p>Welcome to our space where innovation meets simplicity! 
+        We are a passionate community of learners, creators, and 
+        developers dedicated to empowering individuals like you to 
+        build better digital experiences. Whether you're here to learn, 
+        collaborate, or create, we aim to provide you with the tools,
+        guidance, and support you need to succeed</p>
+    </div>
+    );
+};
+export default About;
---
+++ src/components/home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Home = () => {
+    return <h1>Welcome to the home page </h1>;
+};
+export default Home;
